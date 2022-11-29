140cf0b45088815f64d4b3d0cf3e21009b6076fa
Bianca <bianca.fraccaroli@bandtec.com.br>
2022-11-28 22:18:05 -0300

    testing

--- src/main/java/balancefy/api/application/config/security/CorsConfiguration.java
+++ src/main/java/balancefy/api/application/config/security/CorsConfiguration.java
@@ -14,50 +14,16 @@ import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
-public class CorsConfiguration extends org.springframework.web.cors.CorsConfiguration {
+public class CorsConfiguration extends WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-                        .setAllowedHeaders("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowedMethods("*");
     }
 
-    @Bean
-    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-
-        config.setAllowedOrigins(
-                Arrays.asList(
-                        "http://localhost:3000",
-                        "https://balancefy.ddns.net",
-                        "http://balancefy.ddns.net",
-                        "https://api-balancefy.ddns.net",
-                        "http://api-balancefy.ddns.net",
-                        "http://lb-frontend-nginx",
-                        "https://lb-frontend-nginx",
-                        "http://frontend1",
-                        "https://frontend1",
-                        "http://frontend2",
-                        "https://frontend2",
-                        "http://localhost",
-                        "https://localhost"
-                )
-        );
-
-        config.setAllowedMethods(Collections.singletonList("*"));
-        config.setAllowedHeaders(Collections.singletonList("*"));
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return bean;
-    }
 }
